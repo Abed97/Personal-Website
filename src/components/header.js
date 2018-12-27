e09f34f4303f765cff87833bef005ff2ba14c9b8
Abed Atassi <abed.atassi@mail.mcgill.ca>
2018-12-27 10:15:54 -0500

    home page half implemented

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ const Header = ({ siteTitle }) => (
   <div className="HeaderGroup">
     <Link to="/"><img width="30" src={require('../images/alphabet.svg')} /></Link>
     <Link to="/courses">Courses</Link>
-    <Link to="/downloads">Downloads</Link>
+    <Link to="/downloads">Resume</Link>
     <Link to="/projects">Projects</Link>
     <Link to="/contact"><button>Contact</button></Link>
   </div>
